2d95f912d5e1349a6ff099466ed373d9ec3625f8
jnitroun-hue <jnitroun.hue@gmail.com>
2025-11-02 16:15:42 +0300

    fix: removed onBack prop from ProperMultiplayer

--- src/app/multiplayer/page.tsx
+++ src/app/multiplayer/page.tsx
@@ -1,14 +1,7 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
 import ProperMultiplayer from '../../components/ProperMultiplayer';
 
 export default function MultiplayerPage() {
-  const router = useRouter();
-
-  const handleBack = () => {
-    router.push('/');
-  };
-
-  return <ProperMultiplayer onBack={handleBack} />;
+  return <ProperMultiplayer />;
 }
