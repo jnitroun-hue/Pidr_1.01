055080ad078bd2218cb16502422d21c1c61468d9
jnitroun-hue <jnitroun.hue@gmail.com>
2025-11-18 20:43:23 +0300

    REBRAN 1823

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,11 @@ export default function RootLayout({
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
           rel="stylesheet"
           media="print"
-          onLoad="this.media='all'"
+          onLoad={(e) => {
+            if (e.target instanceof HTMLLinkElement) {
+              e.target.media = 'all';
+            }
+          }}
         />
         {/* ✅ ПРЕФЕТЧИНГ ДЛЯ УСКОРЕНИЯ ЗАГРУЗКИ */}
         <link rel="preconnect" href="https://telegram.org" />
